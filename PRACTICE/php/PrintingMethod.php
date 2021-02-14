ce2d4d1f97941f0dbe6c7b52f6afa5991254dee9
Atanusaha143 <atanu.saha415@gmail.com>
2021-02-14 22:55:40 +0600

    Printing methods

---
+++ PRACTICE/php/PrintingMethod.php
@@ -0,0 +1,13 @@
+<?php
+
+	print "Printing with Statement"; // has return value, slower, don't supports multiple parameter 
+	print "\r\n"; // printing new line
+	echo "Printing with echo"; // no return value, faster, supports multiple parameter
+	print "\r\n"; // printing new line
+	echo "This is"." String "."Concatenation"; // Concates String
+	echo "\r\n"; // printing new line with 
+	echo "Passing"," Multiple String ","As Parameter"," with 'echo'"; // Multiple String Printing
+	print("\r\n");
+	print("This is Print Function"); // Single print function
+
+?>
