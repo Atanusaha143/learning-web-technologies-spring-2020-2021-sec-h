8f006b06cbeac06e2963a7d115a5629121263c84
Atanusaha143 <atanu.saha415@gmail.com>
2021-02-15 17:33:29 +0600

    Odd or Even

---
+++ MID_LAB_TASK_PHP_04/3.php
@@ -0,0 +1,17 @@
+<?php
+	
+	$number1 = 2;
+	$number2 = 3;
+
+	echo "Checking $number1... \r\n";
+	echo "Answer: ";
+	if($number1%2 === 0) echo "Even Number";
+	else echo "Odd Number";
+
+	echo "\r\n";
+	echo "Checking $number2... \r\n";
+	echo "Answer: ";
+	if($number2%2 === 0) echo "Even Number";
+	else echo "Odd Number";
+
+?>
