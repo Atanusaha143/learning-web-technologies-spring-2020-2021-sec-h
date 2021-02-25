d2f802aca1e2fcf2e9bc8dba7f59423458874e17
Atanusaha143 <atanu.saha415@gmail.com>
2021-02-25 12:40:04 +0600

    updated home configs

--- MID_LAB_TASK_PHP_SESSION_07/home.php
+++ MID_LAB_TASK_PHP_SESSION_07/home.php
@@ -7,16 +7,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home page</title>
+	<title>Public Home</title>
 </head>
 <body>
-	<h1>Welcome home, XYZ</h1>
+	<table border="1" width="100%" cellspacing="0">
+		<tr>
+			<td align="right" colspan="3">
+				<a href="home.php"> <img src="logo.png" align="left"> </a>
+				Logged in as <?php print_r($_SESSION['name']) ?>
+				&nbsp | &nbsp
+				<a href=""> Logout</a>
+			</td>
+		</tr>
+		<tr height = "200px">
+			<td width="33%">
+				<h4> &nbsp &nbsp &nbsp Account </h4>
+				<hr width="90%">
+				<ul>
+					<li> Dashboard </li>
+					<li> View Profile </li>
+					<li> Edit Profile </li>
+					<li> Change Profile Picture </li>
+					<li> Change Password </li>
+					<li> Logout </li>
+				</ul>
+			</td>
+			<td colspan="2">
+				<h3>
+					&nbsp &nbsp Welcome <?php print_r($_SESSION['name']) ?>
+				</h3> 
+			</td>
+		</tr>
+		<tr height = "50px">
+			<td colspan="3">
+				<center> Copyright &copy 2017 </center>
+			</td>
+		</tr>
+	</table>
 </body>
 </html>
 
 <?php
 
 	}else{
+		echo "Please do Registration before login in";
 		header('location: registration.php');
 	}
 
