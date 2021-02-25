f22d598b6fa24dc5095c1ab0288d020a0c35ba57
Atanusaha143 <atanu.saha415@gmail.com>
2021-02-26 03:15:37 +0600

    updated home for valid user

--- MID_LAB_TASK_PHP_SESSION_07/home.php
+++ MID_LAB_TASK_PHP_SESSION_07/home.php
@@ -16,7 +16,8 @@
 				<a href="home.php"> <img src="logo.png" align="left"> </a>
 				Logged in as <?php print_r($_SESSION['name']) ?>
 				&nbsp | &nbsp
-				<a href="publicHome.html"> Logout</a>
+				<a href="logout.php"> Logout</a>
+				&nbsp
 			</td>
 		</tr>
 		<tr height = "200px">
@@ -24,12 +25,12 @@
 				<h4> &nbsp &nbsp &nbsp Account </h4>
 				<hr width="90%">
 				<ul>
-					<li> Dashboard </li>
+					<li> <a href="home.php"> Dashboard </a></li>
 					<li> <a href="viewProfile.php"> View Profile </a></li>
 					<li> <a href="editProfile.php"> Edit Profile </a> </li>
-					<li> Change Profile Picture </li>
-					<li> Change Password </li>
-					<li> <a href="publicHome.html"> Logout </a> </li>
+					<li> <a href="changeProfilePicture.php"> Change Profile Picture </a> </li>
+					<li> <a href="changePassword.php"> Change Password </a> </li>
+					<li> <a href="logout.php"> Logout </a> </li>
 				</ul>
 			</td>
 			<td colspan="2">
