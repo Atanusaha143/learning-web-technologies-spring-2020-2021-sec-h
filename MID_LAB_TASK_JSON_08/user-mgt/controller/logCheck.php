a7133c957e6b688fc3ace60266c4cac2045ce27d
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-02 17:17:40 +0600

    updated login validation

--- MID_LAB_TASK_JSON_08/user-mgt/controller/logCheck.php
+++ MID_LAB_TASK_JSON_08/user-mgt/controller/logCheck.php
@@ -1,17 +1,20 @@
 <?php
 	session_start();
-
+	
 	if(isset($_POST['submit'])){
 
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+		$userFile = fopen("../model/userValidationInfo.json", "r");
+		$userData = fread($userFile, filesize('../model/userValidationInfo.json'));
+		$userInfo = json_decode($userData, true);
+
+		$username = $userInfo['user'];
+		$password = $userInfo['pass'];
 
 		if($username == "" || $password == ""){
 			echo "null submission...";
 		}else{
-			$user = $_SESSION['current_user'];
 
-			if($username == $user['username'] && $password == $user['password']){
+			if($username == $_POST['username'] && $password == $_POST['password']){
 				$_SESSION['flag'] = true;
 				header('location: ../view/home.php');
 			}else{
