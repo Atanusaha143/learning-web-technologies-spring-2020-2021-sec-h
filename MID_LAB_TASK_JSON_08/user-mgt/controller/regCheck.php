a26ddf7b2d2fceeef1a07f4a836f7cca134bef19
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-02 16:58:37 +0600

    User Management

---
+++ MID_LAB_TASK_JSON_08/user-mgt/controller/regCheck.php
@@ -0,0 +1,32 @@
+<?php
+
+	if(isset($_POST['signup'])){
+
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$repass = $_POST['repass'];
+		$email = $_POST['email'];
+
+		if($username == "" || $email == "" || $password == "" || $repass == ""){
+			echo "null submission...";
+		}else{
+
+			if($password == $repass){
+
+				$userValidationInfo = [
+										 'user' => $username,
+										 'pass' => $password,
+				      				  ];
+
+				$allData = json_encode($userValidationInfo);
+				$userData = fopen("../model/userValidationInfo.json", "a");
+				fwrite($userData, $allData);
+				fclose($userData);
+				header('location: ../view/login.html');
+			}else{
+				echo "password & confirm password mismatch..";
+			}
+		}
+
+	}
+?>
