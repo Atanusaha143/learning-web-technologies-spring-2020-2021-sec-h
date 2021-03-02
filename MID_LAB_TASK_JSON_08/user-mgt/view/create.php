0a92ed717a4d1f164e77aa6c6afb3a8ab79b3a04
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-02 20:51:42 +0600

    updated create funtion

--- MID_LAB_TASK_JSON_08/user-mgt/view/create.php
+++ MID_LAB_TASK_JSON_08/user-mgt/view/create.php
@@ -4,45 +4,67 @@
 ?>
 
 
-	<div id="page_title">
-		<h1>Create Page</h1>
-	</div>
+<div id="page_title">
+	<h1>Create Page</h1>
+</div>
 
-	<div id='nav_bar'>
-		<a href="home.php"> Back</a> |
-		<a href="../controller/logout.php"> logout</a>	
-	</div>
+<div id='nav_bar'>
+	<a href="home.php"> Back</a> |
+	<a href="../controller/logout.php"> logout</a>	
+</div>
 
-	<div id="main_content">
-		<form method="post" action="">
-			<fieldset>
-				<legend>Create New</legend>
-				<table>
-					<tr>
-						<td>Username</td>
-						<td><input type="text" name="username" value=""> </td>
-					</tr>
-					<tr>
-						<td>Password</td>
-						<td><input type="password" name="password" value=""> </td>
-					</tr>
-					<tr>
-						<td>Confirm Password</td>
-						<td><input type="password" name="repass" value=""> </td>
-					</tr>
-					<tr>
-						<td>Email</td>
-						<td><input type="email" name="email" value=""> </td>
-					</tr>
-					<tr>
-						<td></td>
-						<td>
-							<input type="submit" name="signup" value="Create">
-						</td>
-					</tr>
-				</table>
-			</fieldset>
-		</form>
-	</div>
+<div id="main_content">
+	<form method="post" action="">
+		<fieldset>
+			<legend>Create New</legend>
+			<table>
+				<tr>
+					<td>Username</td>
+					<td><input type="text" name="username" value=""> </td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td><input type="password" name="password" value=""> </td>
+				</tr>
+				<tr>
+					<td>Confirm Password</td>
+					<td><input type="password" name="repass" value=""> </td>
+				</tr>
+				<tr>
+					<td>Email</td>
+					<td><input type="email" name="email" value=""> </td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>
+						<input type="submit" name="create" value="Create">
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</div>
 
-	<?php include('footer.php'); ?>
+<?php 
+	include('footer.php'); 
+	if(isset($_POST['create']))
+	{
+		if($_POST['username'] == "" || $_POST['password'] == "" || $_POST['repass'] == "" || $_POST['email'] == ""){
+			echo "null submission...";
+		}else{
+
+				$userValidationInfo = [
+										 'user' => $_POST['username'],
+										 'pass' => $_POST['password'],
+										 'rPass' => $_POST['repass'],
+										 'email' => $_POST['email'],
+	      							  ];
+
+				$allData = json_encode($userValidationInfo);
+				$userData = fopen("../model/userValidationInfo.json", "a");
+				fwrite($userData, $allData);
+				fclose($userData);
+			echo "User created";
+		}
+	}
+?>
