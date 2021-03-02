194d97d20e00cc0da4fc25343058ba8a0a20297f
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-02 17:18:54 +0600

    updated registration validation

--- MID_LAB_TASK_JSON_08/user-mgt/controller/regCheck.php
+++ MID_LAB_TASK_JSON_08/user-mgt/controller/regCheck.php
@@ -19,7 +19,7 @@
 				      				  ];
 
 				$allData = json_encode($userValidationInfo);
-				$userData = fopen("../model/userValidationInfo.json", "a");
+				$userData = fopen("../model/userValidationInfo.json", "w");
 				fwrite($userData, $allData);
 				fclose($userData);
 				header('location: ../view/login.html');
