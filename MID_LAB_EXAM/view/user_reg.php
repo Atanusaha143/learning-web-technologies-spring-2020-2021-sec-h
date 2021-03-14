9db20c8ba123b9222b9423c4e2facc18fe0006e3
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-14 12:30:27 +0600

    1st commit at 12:30 PM

---
+++ MID_LAB_EXAM/view/user_reg.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<form>
+		<fieldset style="width: 15%">
+			<legend>
+				<b>REGISTRATION</b>
+			</legend>
+			<table>
+				<tr>
+					<td>
+						Id
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="text" name="id">
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Password
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="password" name="password">
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Confirm Password
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="password" name="cPassword">
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Name
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="name" name="name">
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						User Type
+					</td>
+				</tr>
+				<tr>
+					<td> <hr> </td>
+				</tr>
+				<tr>
+					<td>
+						<input type="radio" name="user"> User
+						<input type="radio" name="user"> Admin
+					</td>
+				</tr>
+
+				<tr>
+					<td> <hr> </td>
+				</tr>
+
+				<tr>
+					<td>
+						<input type="submit" name="signUp" value="Sign Up">
+					</td>
+					<td>
+						<a href=""> Sign In </a>
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>
