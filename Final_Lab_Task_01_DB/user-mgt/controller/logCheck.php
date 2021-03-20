874deecf92377e3cda1cbbee84dbabb34b7909be
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-20 15:34:07 +0600

    remove unnecessary comments

--- Final_Lab_Task_01_DB/user-mgt/controller/logCheck.php
+++ Final_Lab_Task_01_DB/user-mgt/controller/logCheck.php
@@ -10,16 +10,6 @@
 			$connection = mysqli_connect('localhost','root','','user-mgt');
 			$sql = "select * from registration";
 			$result = mysqli_query($connection, $sql);
-			// $row = mysqli_fetch_assoc($result);
-			// $username = $row['username'];
-			// $password = $row['password'];
-
-			// if($username == $_POST['username'] && $password == $_POST['password']){
-			// 	$_SESSION['flag'] = true;
-			// 	header('location: ../view/home.php');
-			// }else{
-			// 	echo "invalid user";
-			// }
 
 			$check = false;
 			while($row = mysqli_fetch_assoc($result))
