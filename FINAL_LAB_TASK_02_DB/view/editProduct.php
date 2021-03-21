dbfe711bab47d4151d61e73443499e0ea96544ae
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-21 20:21:57 +0600

    updated edit product functionality

--- FINAL_LAB_TASK_02_DB/view/editProduct.php
+++ FINAL_LAB_TASK_02_DB/view/editProduct.php
@@ -1,57 +1,49 @@
 <?php
+	session_start();
 	require_once('../model/dbConnection.php');
 	require_once('../model/productModel.php');
 	$id = $_GET['id'];
 	$connection = getConnection();
 	$selectedProduct = getProductById($id);
+	$_SESSION['id'] = $id;
 ?>
 
 <div id="main_content">
-	<form method="post" action="">
-		<fieldset>
+	<form method="post" action="../controller/updateCheck.php">
+		<fieldset style="width: 10%">
 			<legend>Edit</legend>
 			<table>
 				<tr>
 					<td>Name</td>
+				</tr>
+				<tr>
 					<td><input type="text" name="name" value="<?php echo $selectedProduct['name'] ?>"> </td>
 				</tr>
 				<tr>
 					<td>Buying Price</td>
+				</tr>
+				<tr>
 					<td><input type="text" name="bPrice" value="<?php echo $selectedProduct['buyingPrice'] ?> "> </td>
 				</tr>
-			
 				<tr>
 					<td>Selling Price</td>
-					<td><input type="text" name="sPrice" value="<?php echo $selectedProduct['sellingPrice'] ?> "> </td>
 				</tr>
 				<tr>
-					<td></td>
+					<td><input type="text" name="sPrice" value="<?php echo $selectedProduct['sellingPrice'] ?> "> </td>
+				</tr>
+				<tr><td><hr></td></tr>
+				<tr>
 					<td>
-						<input type="submit" name="update" value="Update">
+						<input type="checkbox" name="check" <?php if($selectedProduct['displayable'] == 'Yes') { echo "checked";} ?>  > Display
+					</td>
+				</tr>
+				<tr><td><hr></td></tr>
+				<tr>
+					<td>
+						<input type="submit" name="update" value="Save">
 					</td>
 				</tr>
 			</table>
 		</fieldset>
 	</form>
 </div>
-
-<?php 
-	if(isset($_POST['update']))
-	{
-		// update database
-		$name = $_POST['name'];
-		$bPrice = $_POST['bPrice'];
-		$sPrice = $_POST['sPrice'];
-		$productDetails = array('name' => $name, 'buyingPrice' => $bPrice, 'sellingPrice' => $sPrice);
-		$connection = getConnection();
-		$check = updateProduct($productDetails);
-		if($check)
-		{
-			echo "Product updated!";
-		}
-		else
-		{
-			echo "Error occured!";
-		}
-	}
-?>
