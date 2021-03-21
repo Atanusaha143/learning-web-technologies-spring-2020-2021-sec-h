d27f07b3465bba097caab4d7a2eea171dab0cde4
Atanusaha143 <atanu.saha415@gmail.com>
2021-03-21 20:21:43 +0600

    updated delete product functionality

--- FINAL_LAB_TASK_02_DB/view/deleteProduct.php
+++ FINAL_LAB_TASK_02_DB/view/deleteProduct.php
@@ -1,39 +1,23 @@
 <?php
+	session_start();
 	require_once('../model/dbConnection.php');
 	require_once('../model/productModel.php');
 	$id = $_GET['id'];
 	$connection = getConnection();
 	$selectedProduct = getProductById($id);
+	$_SESSION['id'] = $id;
 ?>
 
 <div id="main_content">
-	<form method="post" action="">
-		<table>
-			<tr>
-				<td> <?php echo "Are you sure to delete {$selectedProduct['name']}?" ?> </td>
-			</tr>
-			<tr>
-				<td>
-					<input type="submit" name="delete" value="Delete">
-				</td>
-			</tr>
-		</table>
+	<form method="POST" action="../controller/deleteCheck.php">
+		<fieldset style="width: 20%">
+			<legend>DELETE PRODUCT</legend>
+			Name: <?php echo $selectedProduct['name']; ?> <br>
+			Buying Price: <?php echo $selectedProduct['buyingPrice']; ?> <br>
+			Selling Price: <?php echo $selectedProduct['sellingPrice']; ?> <br>
+			Displayable: <?php  if($selectedProduct['displayable'] == 'Yes') { echo "Yes";} else { echo "No";} ?>
+			<hr>
+			<input type="submit" name="delete" value="Delete">
+		</fieldset>
 	</form>
 </div>
-
-<?php 
-	if(isset($_POST['delete']))
-	{
-		$id = $_GET['id'];
-		$connection = getConnection();
-		$check = deleteProduct($id);
-		if($check)
-		{
-			echo "Product deleted!";
-		}
-		else
-		{
-			echo "Error occured!";
-		}
-	}
-?>
